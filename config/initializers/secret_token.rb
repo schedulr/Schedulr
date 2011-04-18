ca35f1f89ec6964ed71986b5867ea20222d333e7
AJ Palkovic <aj.palkovic@gmail.com>
2011-04-18 02:55:56 -0400

    Converted Line Endings to LF.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file.
+++# Be sure to restart your server when you modify this file.
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
