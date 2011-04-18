8949bb422b8501156814034d8462d0ef381dc808
AJ Palkovic <aj.palkovic@gmail.com>
2011-04-18 17:25:57 -0400

    Oops :(

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-++# Be sure to restart your server when you modify this file.
+# Be sure to restart your server when you modify this file.
 
 #Schedulr::Application.config.session_store :cookie_store, :key => '_Schedulr_session'
 
